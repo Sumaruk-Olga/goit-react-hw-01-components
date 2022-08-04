a4bb9b95c81379865139c70c9393a007442f90d3
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-08-04 12:33:08 +0300

    transactionHistory done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import { TransactionHistory } from 'components/TransactionHistory/TransactionHis
 import user from '../../data/user.json';
 import data from '../../data/data.json';
 import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
 
 export const App = () => {
   return (
@@ -29,7 +30,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
-      <TransactionHistory/>
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,3 +1,22 @@
-export const TransactionHistory = () => {
-    return <div>TransactionHistory</div>
+import { Transaction } from "components/Transaction/Transaction"
+
+export const TransactionHistory = ({ items }) => {
+    return <table className="transaction-history">
+    <thead>
+        <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        {items.map(({id, type, amount, currency}) => <Transaction
+            key={id}
+            type={type}
+            amount={amount}
+            currency={currency} />)
+        }  
+    </tbody>
+</table>
 }
