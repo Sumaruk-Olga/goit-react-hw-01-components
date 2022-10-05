0f4a168a07a6be86f0825e9c9aa5bd7ff4dc2446
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-10-05 21:52:09 +0300

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,10 +1,10 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`    
-    display: 'flex';
+    display: flex;
     height: 100%;
     width: 100%;
-    flex-direction: 'column';    
+    flex-direction: column;    
     font-size: ${p => p.theme.fontSizes.m};
     color: ${p => p.theme.text.primary};
     background-color: ${p => p.theme.colors.primary};
